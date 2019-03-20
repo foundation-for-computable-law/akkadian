e96b69aa5be08b606d4a29a6951427e0c01ec3fe
benyoung91 <thebenyoung@gmail.com>
2019-03-20 10:46:17 -0400

    Update sandbox.py
    Adding small ruleset for testing.

--- Engine/sandbox.py
+++ Engine/sandbox.py
@@ -20,13 +20,72 @@ def is_qualifying_relative(a, b): return (
         Not(relationship(a, b) == "Child"))
     )
 
+#evaluate single person for cash benefit, eligible if there is at least one child, 1 or fewer adults
+def is_eligible_cash_benefit(person, household):
+    numAdults = 0
+    numChildren = 0
+
+    #nested function specific to cash benefit
+    def is_child(person, household):
+        focal = person
+        if age(focal) > 17:
+            return False 
+        #if < 18 and is a child of someone in the household then true
+        for hhm in household:
+            if relationship(focal, hhm) == 'Child':
+                print(focal + " is a child")
+                return True
+    
+    #nested function specific to cash benefit   
+    def is_parent(person, household):
+        focal = person
+        for hhm in household:
+            if relationship(focal, hhm) == 'Parent' & relationship(focal, hhm) != None :
+                print(focal + " is a parent of " + hhm)
+                return True
+    
+    def under_19(person): return (
+        (age(person) == 18) |
+            ((age(person) < 18) & 
+             ~(age(person) >= 19)))
+
+    #get count of children and adults
+    for person in household:
+        if(age(person) < 18 & is_child(person, household)):
+            numChildren += 1
+        if(age(person) > 18 | is_parent(person, household)):
+            numAdults += 1
+
+    #if under 18 or (just for testing) not greater than or equal to 19 or equal to 18 and 0 or more adults and 1 or more children
+    return (under_19(person) & 
+        numAdults >= 0 & 
+        numChildren > 1
+        )
+        
+
+def is_eligible_food_benefit(person):
+    return False
+
+def is_eligible_health_benefit(person):
+    return False
+
+#takes list of household members and evalutes whether anyone in the household list is eligible for a benefit
+def household_has_eligible_member(household): 
+
+    for person in household:
+        return (
+            is_eligible_cash_benefit(person, household) | 
+            (is_eligible_food_benefit(person) & is_eligible_health_benefit(person))
+        )
+    return False
+
 
 # Base-level facts from the rules
 # These lookup facts from the fact list
 def age(p): return fact("num", "age", p)
 def gender(p): return fact("str", "gender", p)
 def relationship(a, b): return fact("str", "relationship", a, b)
-
+def citizenship(p): return fact("citizenship", p)
 
 # FACTS
 
@@ -42,7 +101,8 @@ def relationship(a, b): return fact("str", "relationship", a, b)
 
 # Invoking rules
 #is_qualifying_relative("jim","jane").value
-
+household = ["jim", "jane"]
+print(household_has_eligible_member(household))
 
 # Apply the rules to a fact pattern
 # apply_rules('sandbox.is_qualifying_relative("jim","jane")',
