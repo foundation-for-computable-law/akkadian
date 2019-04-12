35df569535a29d4d111cf3cd90bac5b62307524a
benyoung91 <thebenyoung@gmail.com>
2019-04-12 11:01:30 -0400

    fixing investigate call
    fixing investigate call

--- Engine/interview.py
+++ Engine/interview.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from rules import *
+from rules import FormW4Rules
 from dsl import *
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # set FLASK_APP=interview.py
 @app.route("/")
 def hello():
-    return Investigate(['FormW4Rules.form_w4_complete("Husband", "Wife")'])
+    return Investigate([(FormW4Rules.form_w4_complete, "Husband", "Wife")])
 
 
     
